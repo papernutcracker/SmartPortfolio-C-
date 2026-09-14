09ad29ebb0d9581c2ae34df72a16d09f99399ef9
Your Name <mail@gmail.com>
2026-09-14 16:30:14 +0300

    feat: add IBKR PDF report import and clear portfolio button

--- obj/Debug/net10.0/Smart Dividend Portfolio Tracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/Smart Dividend Portfolio Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smart Dividend Portfolio Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7319d63a97f3839aa2d93659b47a231c02b9c1bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dba60e6a0edd7ee2975f76dafbb9dd7c948aa51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smart Dividend Portfolio Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smart Dividend Portfolio Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
